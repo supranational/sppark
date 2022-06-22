3b229e221fccbdcff8ccff02e9accb695257697d
Andy Polyakov <appro@cryptogams.org>
2022-06-22 16:13:52 +0200

    msm/pippenger.cuh: add include-once guards.

--- msm/pippenger.cuh
+++ msm/pippenger.cuh
@@ -2,6 +2,9 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+#ifndef __PIPPENGER_CUH__
+#define __PIPPENGER_CUH__
+
 #include <cuda.h>
 
 #ifndef WARP_SZ
@@ -473,4 +476,5 @@ RustError mult_pippenger(point_t *out, const affine_t points[], size_t npoints,
     return RustError{cudaSuccess};
 }
 
+#endif
 #endif
