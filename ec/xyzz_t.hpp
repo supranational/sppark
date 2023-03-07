dd8b20bd433f6066b9e326c2e2969b530c0679f2
Andy Polyakov <appro@cryptogams.org>
2023-03-07 13:41:16 +0100

    ec/xyzz_t.hpp: add code-size-optimized uadd() methods.

--- ec/xyzz_t.hpp
+++ ec/xyzz_t.hpp
@@ -169,6 +169,129 @@ public:
 #endif
     }
 
+#ifdef __CUDA_ARCH__
+    __device__ void uadd(const xyzz_t& p2)
+    {
+        xyzz_t p31 = *this;
+
+        if (p2.is_inf()) {
+            return;
+        } else if (p31.is_inf()) {
+            *this = p2;
+            return;
+        }
+
+        field_t A, B, U, S, P, R, PP;
+        int pc = -1;
+        bool done = false, dbl = false, inf = false;
+
+        A = p31.Y;
+        B = p2.ZZZ;
+        #pragma unroll 1
+        do {
+            A = A * B;
+            switch (++pc) {
+            case 0:
+                S = A;                  /* S1 = Y1*ZZZ2 */
+                A = p2.Y;
+                B = p31.ZZZ;
+                break;
+            case 1:                     /* S2 = Y2*ZZZ1 */
+                R = A - S;              /* R = S2-S1 */
+                A = p31.X;
+                B = p2.ZZ;
+                break;
+            case 2:
+                U = A;                  /* U1 = X1*ZZ2 */
+                A = p2.X;
+                B = p31.ZZ;
+                break;
+            case 3:                     /* U2 = X2*ZZ1 */
+                A = A - U;              /* P = U2-U1 */
+                inf = A.is_zero();      /* X1==X2, not add |p1| and |p2| */
+                dbl = R.is_zero() & inf;
+                if (dbl) {              /* X1==X2 && Y1==Y2, double |p2| */
+                    A = p2.Y<<1;        /* U = 2*Y1 */
+                    inf = false;        /* don't set |p3| to infinity */
+                }
+                B = A;
+                break;
+            case 4:
+                PP = A;                 /* PP = P^2 */
+                break;
+            case 5:
+#define PPP P
+                PPP = A;                /* PPP = P*PP */
+                B = field_t::csel(field_t::one(), p31.ZZZ, dbl);
+                break;
+            case 6:                     /* ZZZ1*PPP */
+                B = czero(p2.ZZZ, inf);
+                break;
+            case 7:
+                p31.ZZZ = A;            /* ZZZ3 = ZZZ1*ZZZ2*PPP */
+                A = field_t::csel(field_t::one(), p31.ZZ, dbl);
+                B = czero(p2.ZZ, inf);
+                break;
+            case 8:                     /* ZZ1*ZZ2 */
+                B = PP;
+                break;
+            case 9:
+                p31.ZZ = A;             /* ZZ3 = ZZ1*ZZ2*PP */
+                A = field_t::csel(p2.X, U, dbl);
+                break;
+            case 10:
+#define Q PP
+                Q = A;                  /* Q = U1*PP */
+                A = field_t::csel(p2.Y, S, dbl);
+                B = PPP;
+                break;
+            case 11:
+                p31.Y = A;              /* S1*PPP */
+                A = R;
+                B = R;
+                break;
+            case 12:                    /* R^2 */
+                p31.X = A - PPP;        /* R^2-PPP */
+                p31.X -= Q;
+                p31.X -= Q;             /* X3 = R^2-PPP-2*Q */
+                A = Q - p31.X;
+                break;
+            case 13:                    /* R*(Q-X3) */
+                //p31.Y = A - p31.Y;    /* Y3 = R*(Q-X3)-S1*PPP */
+                if (dbl) {
+                    A = p2.X;
+                    B = p2.X;
+                } else {
+                    done = true;
+                }
+                break;
+#undef PPP
+#undef Q
+            /*** double |p2|, V*X1, W*Y1, ZZZ3 and ZZ3 are already calculated ***/
+#define S PP
+            case 14:
+                A = A + A + A;          /* M = 3*X1^2[+a*ZZ1^2] */
+                B = A;
+                break;
+            case 15:
+                p31.X = A - S - S;      /* X3 = M^2-2*S */
+                A = S - p31.X;
+                break;
+            case 16:                    /* M*(S-X3) */
+                //p31.Y = A - p31.Y;    /* Y3 = M*(S-X3)-W*Y1 */
+                done = true;
+                break;
+#undef S
+            }
+        } while (!done);
+        p31.Y = A - p31.Y;              /* Y3 = R*(Q-X3)-S1*PPP */
+
+        *this = p31;
+    }
+#else
+    inline void uadd(const xyzz_t& p2) { add(p2); }
+#endif
+
     /*
      * http://hyperelliptic.org/EFD/g1p/auto-shortw-xyzz.html#addition-madd-2008-s
      * http://hyperelliptic.org/EFD/g1p/auto-shortw-xyzz.html#doubling-mdbl-2008-s-1
@@ -247,5 +370,121 @@ public:
         *this = p31;
 #endif
     }
+
+#ifdef __CUDA_ARCH__
+    template<class affine_t>
+    __device__ void uadd(const affine_t& p2, bool subtract = false)
+    {
+
+        if (p2.is_inf()) {
+            return;
+        } else if (is_inf()) {
+            X = p2.X;
+            Y = p2.Y;
+            ZZ = ZZZ = field_t::one();
+            ZZZ.cneg(subtract);
+            return;
+        }
+
+        xyzz_t p31 = *this;
+
+        field_t A, B, R, PP, PPP;
+        int pc = -1;
+        bool done = false, dbl = false, inf = false;
+
+        A = p2.Y;
+        B = p31.ZZZ;
+        #pragma unroll 1
+        do {
+            A = A * B;
+            switch (++pc) {
+            case 0:                     /* S2 = Y2*ZZZ1 */
+                A.cneg(subtract);
+                R = A - p31.Y;          /* R = S2-Y1 */
+                A = p2.X;
+                B = p31.ZZ;
+                break;
+            case 1:                     /* U2 = X2*ZZ1 */
+                A -= p31.X;             /* P = U2-X1 */
+                inf = A.is_zero();      /* X1==X2, not add |p2| to |p1| */
+                dbl = R.is_zero() & inf;
+                if (dbl) {              /* X1==X2 && Y1==Y2, double |p2| */
+                    A = p2.Y<<1;        /* U = 2*Y1 */
+                    inf = false;        /* don't set |p3| to infinity */
+                }
+                B = A;
+                break;
+            case 2:
+                PP = A;                 /* PP = P^2 */
+                break;
+            case 3:
+                PPP = A;                /* PPP = P*PP */
+                B = czero(p31.ZZZ, inf);
+                break;
+            case 4:
+                p31.ZZZ = field_t::csel(PPP, A, dbl);   /* ZZZ3 = ZZZ1*PPP */
+                A = field_t::csel(p2.Y, p31.Y, dbl);
+                B = PPP;
+                break;
+            case 5:
+                p31.Y = A;              /* Y1*PPP */
+                A = czero(p31.ZZ, inf);
+                B = PP;
+                break;
+            case 6:
+                p31.ZZ = field_t::csel(PP, A, dbl);     /* ZZ3 = ZZ1*PP */
+                A = field_t::csel(p2.X, p31.X, dbl);
+                break;
+            case 7:
+                B = R;
+#define Q R
+                Q = A;                  /* Q = X1*PP */
+                A = B;
+                break;
+            case 8:                     /* R^2 */
+                p31.X = A - PPP;        /* R^2-PPP */
+                p31.X -= Q;
+                p31.X -= Q;             /* X3 = R^2-PPP-2*Q */
+                A = Q - p31.X;
+                break;
+            case 9:                     /* R*(Q-X3) */
+                //p31.Y = A - p31.Y;    /* Y3 = R*(Q-X3)-Y1*PPP */
+                if (dbl) {
+                    //p31.ZZ = PP;      /* [ZZ3 =] V = U^2 */
+                    //p31.ZZZ = PPP;    /* [ZZZ3 =] W = U*V */
+                    A = p2.X;
+                    B = p2.X;
+                } else {
+                    done = true;
+                }
+                break;
+#undef Q
+            /*** double |p2|, V*X1, W*Y1, ZZ3 and ZZZ3 are already calculated ***/
+#define S R
+            case 10:                    /* X1^2 */
+                A += A<<1;              /* M = 3*X1^2[+a] */
+                B = A;
+                break;
+            case 11:                    /* M^2 */
+                p31.X = A - S - S;      /* X3 = M^2-2*S */
+                A = S - p31.X;
+                break;
+            case 12:                    /* M*(S-X3) */
+                //p31.Y = A - p31.Y;    /* Y3 = M*(S-X3)-W*Y1 */
+                p31.ZZZ.cneg(subtract);
+                done = true;
+                break;
+#undef S
+            }
+        } while (!done);
+        p31.Y = A - p31.Y;              /* Y3 = R*(Q-X3)-Y1*PPP */
+
+        *this = p31;
+    }
+#else
+    template<class affine_t>
+    inline void uadd(const affine_t& p2, bool subtract = false)
+    {   add(p2, subtract);   }
+#endif
 };
 #endif
