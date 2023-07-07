06564db6ca075f14cf1149cda44ca73f17cd12e9
Andy Polyakov <appro@cryptogams.org>
2023-07-07 21:16:03 +0200

    rust/src/lib.rs: move Error implementation from the cuda_error macro.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,53 +2,53 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+// Declare C/C++ counterpart as following:
+// extern "C" { fn foobar(...) -> sppark::Error; }
+#[repr(C)]
+pub struct Error {
+    pub code: i32,
+    str: Option<core::ptr::NonNull<i8>>, // just strdup("string") from C/C++
+}
+
+impl Drop for Error {
+    fn drop(&mut self) {
+        extern "C" {
+            fn free(str: Option<core::ptr::NonNull<i8>>);
+        }
+        unsafe { free(self.str) };
+        self.str = None;
+    }
+}
+
+impl From<&Error> for String {
+    fn from(status: &Error) -> Self {
+        if let Some(str) = status.str {
+            let c_str = unsafe { std::ffi::CStr::from_ptr(str.as_ptr()) };
+            String::from(c_str.to_str().unwrap_or("unintelligible"))
+        } else {
+            format!("sppark::Error #{}", status.code)
+        }
+    }
+}
+
+impl From<Error> for String {
+    fn from(status: Error) -> Self {
+        String::from(&status)
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", String::from(self))
+    }
+}
+
 #[macro_export]
 macro_rules! cuda_error {
+    // legacy macro, deprecated
     () => {
-        // Declare C/C++ counterpart as following:
-        // extern "C" { fn foobar(...) -> cuda::Error; }
         mod cuda {
-            #[repr(C)]
-            pub struct Error {
-                pub code: i32,
-                str: Option<core::ptr::NonNull<i8>>, // just strdup("string") from C/C++
-            }
-
-            impl Drop for Error {
-                fn drop(&mut self) {
-                    extern "C" {
-                        fn free(str: Option<core::ptr::NonNull<i8>>);
-                    }
-                    unsafe { free(self.str) };
-                    self.str = None;
-                }
-            }
-
-            impl From<&Error> for String {
-                fn from(status: &Error) -> Self {
-                    let c_str = if let Some(ptr) = status.str {
-                        unsafe { std::ffi::CStr::from_ptr(ptr.as_ptr()) }
-                    } else {
-                        extern "C" {
-                            fn cudaGetErrorString(code: i32) -> *const i8;
-                        }
-                        unsafe { std::ffi::CStr::from_ptr(cudaGetErrorString(status.code)) }
-                    };
-                    String::from(c_str.to_str().unwrap_or("unintelligible"))
-                }
-            }
-
-            impl From<Error> for String {
-                fn from(status: Error) -> Self {
-                    String::from(&status)
-                }
-            }
-
-            impl std::fmt::Display for Error {
-                fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-                    write!(f, "{}", String::from(self))
-                }
-            }
+            pub type Error = sppark::Error;
         }
     };
 }
