5d61cc4ab9d37fd5676b5f225c6eb5b89b9ad563
Andy Polyakov <appro@cryptogams.org>
2023-07-18 12:31:38 +0200

    Add ff/bls12-377-fp2.hpp.

--- ff/bls12-377.hpp
+++ ff/bls12-377.hpp
@@ -25,6 +25,11 @@ namespace device {
         TO_CUDA_T(0x9f7db3a98a7d3ff2), TO_CUDA_T(0x7b4e97b76e7c6305),
         TO_CUDA_T(0x4cf495bf803c84e8), TO_CUDA_T(0x008d6661e2fdf49a)
     };
+    static __device__ __constant__ __align__(16) const uint32_t BLS12_381_Px128[12] = { /* left-aligned value of the modulus */
+        TO_CUDA_T(0x8460000000000080), TO_CUDA_T(0x85aea21800000042),
+        TO_CUDA_T(0x79b117dd04a4000b), TO_CUDA_T(0x116cf9807a89c78f),
+        TO_CUDA_T(0x1d82e03650a49d8d), TO_CUDA_T(0xd71d230be2887563)
+    };
     static __device__ __constant__ /*const*/ uint32_t BLS12_377_M0 = 0xffffffff;
 
     static __device__ __constant__ __align__(16) const uint32_t BLS12_377_r[8] = {
@@ -39,19 +44,25 @@ namespace device {
         TO_CUDA_T(0x7d1c7ffffffffff3), TO_CUDA_T(0x7257f50f6ffffff2),
         TO_CUDA_T(0x16d81575512c0fee), TO_CUDA_T(0x0d4bda322bbb9a9d)
     };
+    static __device__ __constant__ __align__(16) const uint32_t BLS12_377_rx8[8] = { /* left-aligned value of the modulus */
+        TO_CUDA_T(0x508c000000000008), TO_CUDA_T(0xcd53b7f680000008),
+        TO_CUDA_T(0x05a268f2e1bd800a), TO_CUDA_T(0x955b2af4d1652ab3)
+    };
     static __device__ __constant__ /*const*/ uint32_t BLS12_377_m0 = 0xffffffff;
 }
 # ifdef __CUDA_ARCH__   // device-side field types
 # include "mont_t.cuh"
 typedef mont_t<377, device::BLS12_377_P, device::BLS12_377_M0,
-                    device::BLS12_377_RR, device::BLS12_377_one> fp_mont;
+                    device::BLS12_377_RR, device::BLS12_377_one,
+                    device::BLS12_381_Px128> fp_mont;
 struct fp_t : public fp_mont {
     using mem_t = fp_t;
     __device__ __forceinline__ fp_t() {}
     __device__ __forceinline__ fp_t(const fp_mont& a) : fp_mont(a) {}
 };
 typedef mont_t<253, device::BLS12_377_r, device::BLS12_377_m0,
-                    device::BLS12_377_rRR, device::BLS12_377_rone> fr_mont;
+                    device::BLS12_377_rRR, device::BLS12_377_rone,
+                    device::BLS12_377_rx8> fr_mont;
 struct fr_t : public fr_mont {
     using mem_t = fr_t;
     __device__ __forceinline__ fr_t() {}
