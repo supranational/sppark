8daadaa28aeb02e00d48d49b01415bf9715ba537
Andy Polyakov <appro@cryptogams.org>
2023-10-13 12:09:51 +0200

    ff/goldilocks.hpp: remodel.

--- ff/goldilocks.hpp
+++ ff/goldilocks.hpp
@@ -2,27 +2,36 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-#include "gl64_t.cuh"
-
 #ifdef __NVCC__
-# ifdef __CUDA_ARCH__   // device-side field types
-typedef gl64_t fr_t;
-# endif
-#endif
+# include "gl64_t.cuh"  // device-side field types
+# ifndef __CUDA_ARCH__  // host-side stand-in to make CUDA code compile,
+#  include <cstdint>    // not to produce correct result...
 
-#ifndef __CUDA_ARCH__   // host-side stand-in to make CUDA code compile
-#include <cstdint>      // currently only used as a stand-in and should
-class fr_t {            // not be used for any other purpose
+#  if defined(__GNUC__) || defined(__clang__)
+#   pragma GCC diagnostic push
+#   pragma GCC diagnostic ignored "-Wunused-parameter"
+#  endif
+class gl64_t {
     uint64_t val;
 public:
-    inline fr_t()                       {}
-    inline fr_t(uint64_t a) : val(a)    {}
-    inline operator uint64_t() const    { return val; }
-    static inline const fr_t one()      { return 1; }
-    inline fr_t operator+=(fr_t b)      { return val; }
-    inline fr_t operator-=(fr_t b)      { return val; }
-    inline fr_t operator*=(fr_t b)      { return val; }
-    inline fr_t sqr()                   { return val; }
-    inline void zero()                  { val = 0; }
+    using mem_t = gl64_t;
+    static const uint32_t degree = 1;
+    static const uint64_t MOD = 0xffffffff00000001U;
+
+    inline gl64_t()                     {}
+    inline gl64_t(uint64_t a) : val(a)  {}
+    inline operator uint64_t() const    { return val;   }
+    static inline const gl64_t one()    { return 1;     }
+    inline gl64_t& operator+=(gl64_t b) { return *this; }
+    inline gl64_t& operator-=(gl64_t b) { return *this; }
+    inline gl64_t& operator*=(gl64_t b) { return *this; }
+    inline gl64_t& operator^=(int p)    { return *this; }
+    inline gl64_t& sqr()                { return *this; }
+    inline void zero()                  { val = 0;      }
 };
+#  if defined(__GNUC__) || defined(__clang__)
+#   pragma GCC diagnostic pop
+#  endif
+# endif
+typedef gl64_t fr_t;
 #endif
