cc0d7c85230402dc08c5be918b066c9fdb1e7140
Andy Polyakov <appro@cryptogams.org>
2023-10-13 11:46:44 +0200

    ff/baby_bear.hpp: remodel and add some debugging support.

--- ff/baby_bear.hpp
+++ ff/baby_bear.hpp
@@ -2,27 +2,60 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-#include "bb31_t.cuh"
-
 #ifdef __NVCC__
-# ifdef __CUDA_ARCH__   // device-side field types
-typedef bb31_t fr_t;
-# endif
-#endif
+# include "bb31_t.cuh"  // device-side field types
+# ifndef __CUDA_ARCH__  // host-side stand-in to make CUDA code compile,
+#  include <cstdint>    // and provide some debugging support, but
+                        // not to produce correct computational result...
 
-#ifndef __CUDA_ARCH__   // host-side stand-in to make CUDA code compile
-#include <cstdint>      // currently only used as a stand-in and should
-class fr_t {            // not be used for any other purpose
+#  if defined(__GNUC__) || defined(__clang__)
+#   pragma GCC diagnostic push
+#   pragma GCC diagnostic ignored "-Wunused-parameter"
+#  endif
+class bb31_t {
     uint32_t val;
+
+    static const uint32_t M = 0x77ffffff;
 public:
-    inline fr_t()                       {}
-    inline fr_t(uint32_t a) : val(a)    {}
-    inline operator uint32_t() const    { return val; }
-    static inline const fr_t one()      { return 1; }
-    inline fr_t operator+=(fr_t b)      { return val; }
-    inline fr_t operator-=(fr_t b)      { return val; }
-    inline fr_t operator*=(fr_t b)      { return val; }
-    inline fr_t sqr()                   { return val; }
-    inline void zero()                  { val = 0; }
+    using mem_t = bb31_t;
+    static const uint32_t degree = 1;
+    static const uint32_t nbits = 31;
+    static const uint32_t MOD = 0x78000001;
+
+    inline bb31_t()                     {}
+    inline bb31_t(uint32_t a) : val(a)  {}
+    // this is used in constant declaration, e.g. as bb31_t{11}
+    inline constexpr bb31_t(int a) : val(((uint64_t)a << 32) % MOD) {}
+
+    static inline const bb31_t one()            { return bb31_t(1); }
+    inline bb31_t& operator+=(bb31_t b)         { return *this;     }
+    inline bb31_t& operator-=(bb31_t b)         { return *this;     }
+    inline bb31_t& operator*=(bb31_t b)         { return *this;     }
+    inline bb31_t& operator^=(int b)            { return *this;     }
+    inline bb31_t& sqr()                        { return *this;     }
+    friend bb31_t operator+(bb31_t a, bb31_t b) { return a += b;    }
+    friend bb31_t operator-(bb31_t a, bb31_t b) { return a -= b;    }
+    friend bb31_t operator*(bb31_t a, bb31_t b) { return a *= b;    }
+    friend bb31_t operator^(bb31_t a, int b)    { return a ^= b;    }
+    inline void zero()                          { val = 0;          }
+    inline bool is_zero() const                 { return val==0;    }
+    inline operator uint32_t() const
+    {   return ((val*M)*(uint64_t)MOD + val) >> 32;  }
+    inline void to()    { val = ((uint64_t)val<<32) % MOD;  }
+    inline void from()  { val = *this; }
+#  if defined(_GLIBCXX_IOSTREAM) || defined(_IOSTREAM_) // non-standard
+    friend std::ostream& operator<<(std::ostream& os, const bb31_t& obj)
+    {
+        auto f = os.flags();
+        os << "0x" << std::hex << (uint32_t)obj;
+        os.flags(f);
+        return os;
+    }
+#  endif
 };
+#  if defined(__GNUC__) || defined(__clang__)
+#   pragma GCC diagnostic pop
+#  endif
+# endif
+typedef bb31_t fr_t;
 #endif
