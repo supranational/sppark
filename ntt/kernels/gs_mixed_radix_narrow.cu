23e70af2a54e1be79f43390fc7cf4241148678b1
Andy Polyakov <appro@cryptogams.org>
2023-11-15 17:35:32 +0100

    ntt/kernels/*_narrow.cu: de-duplicate kernels.

--- ntt/kernels/gs_mixed_radix_narrow.cu
+++ ntt/kernels/gs_mixed_radix_narrow.cu
@@ -3,176 +3,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 template<unsigned int z_count>
-__launch_bounds__(768, 1) __global__
-void _GS_NTT(const unsigned int radix, const unsigned int lg_domain_size,
-             const unsigned int stage, const unsigned int iterations,
-             fr_t* d_inout, const fr_t (*d_partial_twiddles)[WINDOW_SIZE],
-             const fr_t* d_radix6_twiddles, const fr_t* d_radixX_twiddles,
-             bool is_intt, const fr_t d_domain_size_inverse)
-{
-#if (__CUDACC_VER_MAJOR__-0) >= 11
-    __builtin_assume(lg_domain_size <= MAX_LG_DOMAIN_SIZE);
-    __builtin_assume(radix <= 10);
-    __builtin_assume(iterations <= radix);
-    __builtin_assume(stage <= lg_domain_size && stage >= iterations);
-#endif
-    extern __shared__ fr_t shared_exchange[];
-
-    index_t tid = threadIdx.x + blockDim.x * (index_t)blockIdx.x;
-
-    const index_t diff_mask = (1 << (iterations - 1)) - 1;
-    const index_t inp_mask = ((index_t)1 << (stage - 1)) - 1;
-    const index_t out_mask = ((index_t)1 << (stage - iterations)) - 1;
-
-    const index_t tiz = (tid & ~diff_mask) * z_count + (tid & diff_mask);
-
-    fr_t r[2][z_count];
-
-    // rearrange |tiz|'s bits
-    index_t idx0 = (tiz & ~inp_mask) * 2;
-    idx0 += (tiz << (stage - iterations)) & inp_mask;
-    idx0 += (tiz >> (iterations - 1)) & out_mask;
-    index_t idx1 = idx0 + ((index_t)1 << (stage - 1));
-
-    r[0][0] = d_inout[idx0];
-    r[1][0] = d_inout[idx1];
-
-    unsigned int z_shift = out_mask==0 ? iterations : 0;
-    #pragma unroll
-    for (int z = 1; z < z_count; z++) {
-        r[0][z] = d_inout[idx0 + (z << z_shift)];
-        r[1][z] = d_inout[idx1 + (z << z_shift)];
-    }
-
-    #pragma unroll 1
-    for (int s = iterations; --s >= 6;) {
-        unsigned int laneMask = 1 << (s - 1);
-        unsigned int thrdMask = (1 << s) - 1;
-        unsigned int rank = threadIdx.x & thrdMask;
-        bool pos = rank < laneMask;
-
-        fr_t root = d_radixX_twiddles[rank << (radix - (s + 1))];
-
-        #pragma unroll
-        for (int z = 0; z < z_count; z++) {
-            fr_t t = root * (r[0][z] - r[1][z]);
-            r[0][z] = r[0][z] + r[1][z];
-            r[1][z] = t;
-        }
-
-#ifdef __CUDA_ARCH__
-        fr_t (*xchg)[z_count] = reinterpret_cast<decltype(xchg)>(shared_exchange);
-
-        __syncthreads();
-
-        #pragma unroll
-        for (int z = 0; z < z_count; z++) {
-            fr_t t = fr_t::csel(r[1][z], r[0][z], pos);
-            xchg[threadIdx.x][z] = t;
-        }
-
-        __syncthreads();
-
-        #pragma unroll
-        for (int z = 0; z < z_count; z++) {
-            fr_t t = xchg[threadIdx.x ^ laneMask][z];
-            r[0][z] = fr_t::csel(t, r[0][z], !pos);
-            r[1][z] = fr_t::csel(t, r[1][z], pos);
-        }
-#endif
-    }
-
-    #pragma unroll 1
-    for (int s = min(iterations, 6); --s >= 1;) {
-        unsigned int laneMask = 1 << (s - 1);
-        unsigned int thrdMask = (1 << s) - 1;
-        unsigned int rank = threadIdx.x & thrdMask;
-        bool pos = rank < laneMask;
-
-        fr_t root = d_radix6_twiddles[rank << (6 - (s + 1))];
-
-        #pragma unroll
-        for (int z = 0; z < z_count; z++) {
-            fr_t t = root * (r[0][z] - r[1][z]);
-            r[0][z] = r[0][z] + r[1][z];
-            r[1][z] = t;
-
-#ifdef __CUDA_ARCH__
-            t = fr_t::csel(r[1][z], r[0][z], pos);
-
-            shfl_bfly(t, laneMask);
-
-            r[0][z] = fr_t::csel(t, r[0][z], !pos);
-            r[1][z] = fr_t::csel(t, r[1][z], pos);
-#endif
-        }
-    }
-
-    #pragma unroll
-    for (int z = 0; z < z_count; z++) {
-        fr_t t = r[0][z] - r[1][z];
-        r[0][z] = r[0][z] + r[1][z];
-        r[1][z] = t;
-    }
-
-    if (stage - iterations != 0) {
-        index_t thread_ntt_pos = (tiz & inp_mask) >> (iterations - 1);
-        unsigned int thread_ntt_idx = (tiz & diff_mask) * 2;
-        unsigned int nbits = MAX_LG_DOMAIN_SIZE - (stage - iterations);
-        index_t idx0 = bit_rev(thread_ntt_idx, nbits);
-        index_t root_idx0 = idx0 * thread_ntt_pos;
-        index_t root_idx1 = root_idx0 + (thread_ntt_pos << (nbits - 1));
-
-        fr_t first_root, second_root;
-        get_intermediate_roots(first_root, second_root,
-                               root_idx0, root_idx1, d_partial_twiddles);
-        r[0][0] = r[0][0] * first_root;
-        r[1][0] = r[1][0] * second_root;
-        if (z_count > 1) {
-            fr_t first_root_z = get_intermediate_root(idx0, d_partial_twiddles);
-            unsigned int off = (nbits - 1) / LG_WINDOW_SIZE;
-            unsigned int win = off * LG_WINDOW_SIZE;
-            fr_t second_root_z = d_partial_twiddles[off][1 << (nbits - 1 - win)];
-
-            second_root_z *= first_root_z;
-            #pragma unroll
-            for (int z = 1; z < z_count; z++) {
-                first_root *= first_root_z;
-                second_root *= second_root_z;
-                r[0][z] = r[0][z] * first_root;
-                r[1][z] = r[1][z] * second_root;
-            }
-        }
-    }
-
-    if (is_intt && stage == iterations) {
-        #pragma unroll
-        for (int z = 0; z < z_count; z++) {
-            r[0][z] = r[0][z] * d_domain_size_inverse;
-            r[1][z] = r[1][z] * d_domain_size_inverse;
-        }
-    }
-
-    // rotate "iterations" bits in indices
-    index_t mask = ((index_t)1 << stage) - ((index_t)1 << (stage - iterations));
-    index_t rotw = idx0 & mask;
-    rotw = (rotw << 1) | (rotw >> (iterations - 1));
-    idx0 = (idx0 & ~mask) | (rotw & mask);
-    rotw = idx1 & mask;
-    rotw = (rotw << 1) | (rotw >> (iterations - 1));
-    idx1 = (idx1 & ~mask) | (rotw & mask);
-
-    d_inout[idx0] = r[0][0];
-    d_inout[idx1] = r[1][0];
-
-    #pragma unroll
-    for (int z = 1; z < z_count; z++) {
-        d_inout[idx0 + (z << z_shift)] = r[0][z];
-        d_inout[idx1 + (z << z_shift)] = r[1][z];
-    }
-}
-
-template<unsigned int z_count>
 __device__ __forceinline__
 void gs_coalesced_load(const fr_t* inout, fr_t* r,
                        const index_t offset,
@@ -214,7 +44,7 @@ void gs_coalesced_store(fr_t* inout, const fr_t* r,
                         index_t idx, index_t offset,
                         unsigned int stage, unsigned int iterations)
 {
-    extern __shared__ fr_t shared_mem[][z_count];
+    extern __shared__ fr_t shared_exchange[];
 
     const unsigned int diff_mask = (1 << (iterations - 1)) - 1;
     const index_t out_mask = ((index_t)1 << (stage - 1)) - 1;
@@ -226,7 +56,8 @@ void gs_coalesced_store(fr_t* inout, const fr_t* r,
     offset += thread_ntt_idx + current_ntt_idx;
 
     const unsigned int x = threadIdx.x & (z_count - 1);
-    auto xchg = &shared_mem[threadIdx.x & (0 - z_count)];
+    fr_t (*xchg)[z_count] = reinterpret_cast<decltype(xchg)>(shared_exchange);
+    xchg += threadIdx.x & (0 - z_count);
 
     #pragma unroll
     for (int z = 0; z < z_count; z++) {
@@ -241,13 +72,13 @@ void gs_coalesced_store(fr_t* inout, const fr_t* r,
     }
 }
 
-template<unsigned int z_count>
+template<unsigned int z_count, bool coalesced = false>
 __launch_bounds__(768, 1) __global__
-void _GS_NTT_3D(const unsigned int radix, const unsigned int lg_domain_size,
-                const unsigned int stage, const unsigned int iterations,
-                fr_t* d_inout, const fr_t (*d_partial_twiddles)[WINDOW_SIZE],
-                const fr_t* d_radix6_twiddles, const fr_t* d_radixX_twiddles,
-                bool is_intt, const fr_t d_domain_size_inverse)
+void _GS_NTT(const unsigned int radix, const unsigned int lg_domain_size,
+             const unsigned int stage, const unsigned int iterations,
+             fr_t* d_inout, const fr_t (*d_partial_twiddles)[WINDOW_SIZE],
+             const fr_t* d_radix6_twiddles, const fr_t* d_radixX_twiddles,
+             bool is_intt, const fr_t d_domain_size_inverse)
 {
 #if (__CUDACC_VER_MAJOR__-0) >= 11
     __builtin_assume(lg_domain_size <= MAX_LG_DOMAIN_SIZE);
@@ -265,10 +96,26 @@ void _GS_NTT_3D(const unsigned int radix, const unsigned int lg_domain_size,
 
     const index_t tiz = (tid & ~diff_mask) * z_count + (tid & diff_mask);
 
+    index_t idx0, idx1;
     fr_t r[2][z_count];
 
-    gs_coalesced_load<z_count>(d_inout, r[0], 0, stage, iterations);
-    gs_coalesced_load<z_count>(d_inout, r[1], inp_mask+1, stage, iterations);
+    if (coalesced) {
+        gs_coalesced_load<z_count>(d_inout, r[0], 0, stage, iterations);
+        gs_coalesced_load<z_count>(d_inout, r[1], inp_mask+1, stage, iterations);
+    } else {
+        // rearrange |tiz|'s bits
+        idx0 = (tiz & ~inp_mask) * 2;
+        idx0 += (tiz << (stage - iterations)) & inp_mask;
+        idx0 += (tiz >> (iterations - 1)) & out_mask;
+        idx1 = idx0 + ((index_t)1 << (stage - 1));
+
+        unsigned int z_shift = out_mask==0 ? iterations : 0;
+        #pragma unroll
+        for (int z = 0; z < z_count; z++) {
+            r[0][z] = d_inout[idx0 + (z << z_shift)];
+            r[1][z] = d_inout[idx1 + (z << z_shift)];
+        }
+    }
 
     #pragma unroll 1
     for (int s = iterations; --s >= 6;) {
@@ -379,10 +226,28 @@ void _GS_NTT_3D(const unsigned int radix, const unsigned int lg_domain_size,
         }
     }
 
-    gs_coalesced_store<z_count>
-        (d_inout, r[0], tiz & ((index_t)0 - z_count), 0, stage, iterations);
-    gs_coalesced_store<z_count>
-        (d_inout, r[1], tiz & ((index_t)0 - z_count), out_mask+1, stage, iterations);
+    if (coalesced) {
+        gs_coalesced_store<z_count>
+            (d_inout, r[0], tiz & ((index_t)0 - z_count), 0, stage, iterations);
+        gs_coalesced_store<z_count>
+            (d_inout, r[1], tiz & ((index_t)0 - z_count), out_mask+1, stage, iterations);
+    } else {
+        // rotate "iterations" bits in indices
+        index_t mask = ((index_t)1 << stage) - ((index_t)1 << (stage - iterations));
+        index_t rotw = idx0 & mask;
+        rotw = (rotw << 1) | (rotw >> (iterations - 1));
+        idx0 = (idx0 & ~mask) | (rotw & mask);
+        rotw = idx1 & mask;
+        rotw = (rotw << 1) | (rotw >> (iterations - 1));
+        idx1 = (idx1 & ~mask) | (rotw & mask);
+
+        unsigned int z_shift = out_mask==0 ? iterations : 0;
+        #pragma unroll
+        for (int z = 0; z < z_count; z++) {
+            d_inout[idx0 + (z << z_shift)] = r[0][z];
+            d_inout[idx1 + (z << z_shift)] = r[1][z];
+        }
+    }
 }
 
 template __global__
@@ -395,9 +260,9 @@ void _GS_NTT<Z_COUNT>(unsigned int, unsigned int, unsigned int, unsigned int,
                       fr_t*, const fr_t (*)[WINDOW_SIZE], const fr_t*, const fr_t*,
                       bool, const fr_t);
 template __global__
-void _GS_NTT_3D<Z_COUNT>(unsigned int, unsigned int, unsigned int, unsigned int,
-                         fr_t*, const fr_t (*)[WINDOW_SIZE], const fr_t*, const fr_t*,
-                         bool, const fr_t);
+void _GS_NTT<Z_COUNT, true>(unsigned int, unsigned int, unsigned int, unsigned int,
+                            fr_t*, const fr_t (*)[WINDOW_SIZE], const fr_t*, const fr_t*,
+                            bool, const fr_t);
 
 #ifndef __CUDA_ARCH__
 
@@ -459,7 +324,7 @@ public:
         else if (stage == iterations || lg_domain_size < 12)
             _GS_NTT<Z_COUNT><<<num_blocks/Z_COUNT, block_size, Z_COUNT*shared_sz, stream>>>(NTT_ARGUMENTS);
         else
-            _GS_NTT_3D<Z_COUNT><<<num_blocks/Z_COUNT, block_size, Z_COUNT*shared_sz, stream>>>(NTT_ARGUMENTS);
+            _GS_NTT<Z_COUNT, true><<<num_blocks/Z_COUNT, block_size, Z_COUNT*shared_sz, stream>>>(NTT_ARGUMENTS);
 
         #undef NTT_ARGUMENTS
 
