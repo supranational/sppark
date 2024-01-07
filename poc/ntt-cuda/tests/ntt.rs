ada2ea2103fac3ae6b40a700acafb1f3c9cf6ed8
Andy Polyakov <appro@cryptogams.org>
2024-01-07 22:05:23 +0100

    poc/ntt-cuda: modernize.

--- poc/ntt-cuda/tests/ntt.rs
+++ poc/ntt-cuda/tests/ntt.rs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-use ntt_cuda::NTTInputOutputOrder;
+use sppark::NTTInputOutputOrder;
 
 const DEFAULT_GPU: usize = 0;
 
