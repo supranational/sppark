59cfe091052fcde415f3eef3ed328924bd7992d8
Andy Polyakov <appro@cryptogams.org>
2024-07-11 18:38:08 +0200

    Add Go bridge.

---
+++ poc/go/poc.cu
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+__global__ void kernel()
+{
+    printf("hello from GPU\n");
+}
+
+struct Error {
+    int code;
+    char *message;
+};
+
+extern "C"
+#ifdef _WIN32
+__declspec(dllexport)
+#else
+__attribute__((visibility("default")))
+#endif
+Error cuda_func(void *ptr)
+{
+    kernel<<<1,1>>>();
+    auto err = cudaGetLastError();
+    if (err != cudaSuccess)
+        return {err, strdup(cudaGetErrorString(err))};
+    err = cudaDeviceSynchronize();
+    return {err, strdup(cudaGetErrorString(err))};
+    (void)ptr;
+}
