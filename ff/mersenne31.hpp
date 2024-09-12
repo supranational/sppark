73bcf65d552e50334d13b1602be5ee5f0a9a0f60
Andy Polyakov <appro@cryptogams.org>
2024-09-12 12:14:40 +0200

    ff/mersenne31.hpp: deduplicate some methods.

--- ff/mersenne31.hpp
+++ ff/mersenne31.hpp
@@ -36,20 +36,15 @@ struct mrs31_t : public mrs31_base {
     inline void from()                  { *this >>= 1; }
     inline void store(mem_t* p) const   { *p = *this;  }
 
-    inline mrs31_t reciprocal() const
-    {
-        mrs31_t x05, x0f, x7d, xff, ret = *this;
+    inline mrs31_t& operator<<=(int l);
+    friend inline mrs31_t operator<<(mrs31_t a, int l)
+    {   return a <<= l;   }
 
-        x05 = sqr_n_mul(ret, 2, ret);   // 0b101
-        x0f = sqr_n_mul(x05, 1, x05);   // 0b1111
-        x7d = sqr_n_mul(x0f, 3, x05);   // 0b1111101
-        xff = sqr_n_mul(x7d, 1, x05);   // 0b11111111
-        ret = sqr_n_mul(xff, 8, xff);   // 0b1111111111111111
-        ret = sqr_n_mul(ret, 8, xff);   // 0b111111111111111111111111
-        ret = sqr_n_mul(ret, 7, x7d);   // 0b1111111111111111111111111111101
+    inline mrs31_t& operator>>=(int r);
+    friend inline mrs31_t operator>>(mrs31_t a, int r)
+    {   return a >>= r;   }
 
-        return ret;
-    }
+    inline mrs31_t reciprocal() const;
     friend inline mrs31_t operator/(int one, mrs31_t a)
     {   if (one != 1) asm("trap;"); return a.reciprocal();   }
     friend inline mrs31_t operator/(mrs31_t a, mrs31_t b)
@@ -61,11 +56,10 @@ struct mrs31_t : public mrs31_base {
     {   return sqr_n(*this, 29);   }
     friend inline mrs31_t sqrt(mrs31_t a)
     {   return a.sqrt();   }
+
+    inline mrs31_t pentaroot() const;
 };
 # undef inline
-
-typedef mrs31_t fr_t;
-
 #else
 # include <cstdint>
 # include <cassert>
@@ -122,46 +116,12 @@ public:
     friend inline mrs31_t operator+(mrs31_t a, const mrs31_t b)
     {   return a += b;   }
 
-    inline mrs31_t& operator<<=(uint32_t l)
-    {
-        l %= 31;
-
-        if (l > 2) {
-            uint64_t tmp = (uint64_t)val << l;
-            val = ((uint32_t)tmp & MOD) + (uint32_t)(tmp >> 31);
-            if (val >= MOD)
-                val -= MOD;
-        } else {
-            while (l--) {
-                val <<= 1;
-                if (val >= MOD)
-                    val -= MOD;
-            }
-        }
-
-        return *this;
-    }
-    friend inline mrs31_t operator<<(mrs31_t a, uint32_t l)
+    inline mrs31_t& operator<<=(int l);
+    friend inline mrs31_t operator<<(mrs31_t a, int l)
     {   return a <<= l;   }
 
-    inline mrs31_t& operator>>=(uint32_t r)
-    {
-        r %= 31;
-
-        if (r > 2) {
-            uint32_t red = val & ((1<<r) - 1);
-            uint64_t ret = val + red * (uint64_t)MOD;
-            val = (uint32_t)(ret >> r);
-        } else {
-            while (r--) {
-                val += val&1 ? MOD : 0;
-                val >>= 1;
-            }
-        }
-
-        return *this;
-    }
-    friend inline mrs31_t operator>>(mrs31_t a, uint32_t r)
+    inline mrs31_t& operator>>=(int r);
+    friend inline mrs31_t operator>>(mrs31_t a, int r)
     {   return a >>= r;   }
 
     inline mrs31_t& operator-=(const mrs31_t b)
@@ -361,26 +321,15 @@ public:
     friend inline mrs31_t sqrt(mrs31_t a)
     {   return a.sqrt();   }
 
-    inline mrs31_t reciprocal() const
-    {
-        mrs31_t x05, x0f, x7d, xff, ret = *this;
-
-        x05 = sqr_n_mul(ret, 2, ret);   // 0b101
-        x0f = sqr_n_mul(x05, 1, x05);   // 0b1111
-        x7d = sqr_n_mul(x0f, 3, x05);   // 0b1111101
-        xff = sqr_n_mul(x7d, 1, x05);   // 0b11111111
-        ret = sqr_n_mul(xff, 8, xff);   // 0b1111111111111111
-        ret = sqr_n_mul(ret, 8, xff);   // 0b111111111111111111111111
-        ret = sqr_n_mul(ret, 7, x7d);   // 0b1111111111111111111111111111101
-
-        return ret;
-    }
+    inline mrs31_t reciprocal() const;
     friend inline mrs31_t operator/(int one, mrs31_t a)
     {   assert(one == 1); return a.reciprocal();   }
     friend inline mrs31_t operator/(mrs31_t a, mrs31_t b)
     {   return a * b.reciprocal();   }
     inline mrs31_t& operator/=(const mrs31_t a)
     {   *this *= a.reciprocal(); return *this;   }
+
+    inline mrs31_t pentaroot() const;
 
 # if defined(__CUDACC__)
 #  undef inline
@@ -412,8 +361,63 @@ public:
     }
 # endif
 };
+#endif
 
 typedef mrs31_t fr_t;
 
+#if defined(__CUDACC__) && defined(__SPPARK_FF_MONT32_T_CUH__)
+# define inline __device__ __forceinline__
+#elif defined(__CUDACC__) || defined(__HIPCC__)
+# define inline __host__ __device__ __forceinline__
+#endif
+inline mrs31_t& mrs31_t::operator<<=(int l)
+{
+    l = (l&31) + (l>>5);    // l %= 31 for l<1055 [with below correction]
+    if (l >= 31)
+        l -= 31;
+    val = ((val << l) & 0x7fffffff) | (val >> (31-l));
+    return *this;
+}
+
+inline mrs31_t& mrs31_t::operator>>=(int r)
+{
+    r = (r&31) + (r>>5);    // r %= 31 for r<1055 [with below correction]
+    if (r >= 31)
+        r -= 31;
+    val = ((val << (31-r)) & 0x7fffffff) | (val >> r);
+    return *this;
+}
+
+inline mrs31_t mrs31_t::reciprocal() const
+{
+    mrs31_t x05, x0f, x7d, xff, ret = *this;
+
+    x05 = sqr_n_mul(ret, 2, ret);   // 0b101
+    x0f = sqr_n_mul(x05, 1, x05);   // 0b1111
+    x7d = sqr_n_mul(x0f, 3, x05);   // 0b1111101
+    xff = sqr_n_mul(x7d, 1, x05);   // 0b11111111
+    ret = sqr_n_mul(xff, 8, xff);   // 0b1111111111111111
+    ret = sqr_n_mul(ret, 8, xff);   // 0b111111111111111111111111
+    ret = sqr_n_mul(ret, 7, x7d);   // 0b1111111111111111111111111111101
+
+    return ret;
+}
+
+inline mrs31_t mrs31_t::pentaroot() const
+{
+    mrs31_t x05, x06, x66, ret = *this;
+
+    x05 = sqr_n_mul(ret, 2, ret);   // 0b101
+    x06 = x05 * ret;                // 0b110
+    x66 = sqr_n_mul(x06, 4, x06);   // 0b1100110
+    ret = sqr_n_mul(x66, 8, x66);   // 0b110011001100110
+    ret = sqr_n_mul(ret, 8, x66);   // 0b11001100110011001100110
+    ret = sqr_n_mul(ret, 4, x06);   // 0b110011001100110011001100110
+    ret = sqr_n_mul(ret, 4, x05);   // 0b1100110011001100110011001100101
+
+    return ret;
+}
+#if defined(__CUDACC__) || defined(__HIPCC__)
+# undef inline
 #endif
 #endif /* __SPPARK_FF_MERSENNE31_HPP__ */
