2c2b263fb1cf2c450d12ef1624d10fd9deb1938c
Andy Polyakov <appro@cryptogams.org>
2024-12-06 12:03:19 +0100

    util/gpu_t.cuh: match async malloc with async free in dev_ptr_t.

--- util/gpu_t.cuh
+++ util/gpu_t.cuh
@@ -321,15 +321,16 @@ public:
 // care about freeing it.
 template<typename T> class dev_ptr_t {
     T* d_ptr;
+    cudaStream_t stream;
 public:
-    dev_ptr_t(size_t nelems) : d_ptr(nullptr)
+    dev_ptr_t(size_t nelems) : d_ptr(nullptr), stream(nullptr)
     {
         if (nelems) {
             size_t n = (nelems+WARP_SZ-1) & ((size_t)0-WARP_SZ);
             CUDA_OK(cudaMalloc(&d_ptr, n * sizeof(T)));
         }
     }
-    dev_ptr_t(size_t nelems, stream_t& s) : d_ptr(nullptr)
+    dev_ptr_t(size_t nelems, const stream_t& s) : d_ptr(nullptr), stream(s)
     {
         if (nelems) {
             size_t n = (nelems+WARP_SZ-1) & ((size_t)0-WARP_SZ);
@@ -338,7 +339,13 @@ public:
     }
     dev_ptr_t(const dev_ptr_t& r) = delete;
     dev_ptr_t& operator=(const dev_ptr_t& r) = delete;
-    ~dev_ptr_t() { if (d_ptr) (void)cudaFree((void*)d_ptr); }
+    ~dev_ptr_t()
+    {
+        if (d_ptr) {
+            if (stream) (void)cudaFreeAsync((void*)d_ptr, stream);
+            else        (void)cudaFree((void*)d_ptr);
+        }
+    }
 
     inline operator const T*() const            { return d_ptr; }
     inline operator T*() const                  { return d_ptr; }
